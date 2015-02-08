3bad1df49a453a056f7de9ed0c7d311f5c6a8c49
Uri Goldshtein <uri.goldshtein@gmail.com>
2015-02-08 03:20:15 -0800

    Fixed app initialization, everything is working now

--- client/lib/app.js
+++ client/lib/app.js
@@ -2,3 +2,15 @@
 angular.module('kamerApp', [
     'angular-meteor', 'ui.router', 'ionic']
 );
+
+function onReady() {
+  angular.bootstrap(document, ['kamerApp']);
+}
+
+if (Meteor.isCordova) {
+  angular.element(document).on("deviceready", onReady);
+}
+else {
+  angular.element(document).ready(onReady);
+}
+
